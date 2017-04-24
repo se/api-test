aea76aa1c175bc690df8eb410e90cab186ce92ab
se <selcukermaya@gmail.com>
2017-04-24 17:59:05 +0300

    First upload.

---
+++ Controllers/HookController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_test.Controllers
+{
+    [Route("[controller]")]
+    public class HookController : Controller
+    {
+        [HttpPost]
+        public object Post([FromBody]string value)
+        {
+
+            return default(object);
+        }
+    }
+}
